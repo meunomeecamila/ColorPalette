2e9c1cec4d1d248789ed7928da9f99dad11dbbc2
Camila <contatocamilamenezes@gmail.com>
2026-02-20 12:09:47 -0300

    Alteração IOS

--- assets/js/color-utils.js
+++ assets/js/color-utils.js
@@ -1,6 +1,10 @@
 function hexToRgb(hex) {
     hex = hex.replace("#", "");
 
+    if (hex.length === 3) {
+        hex = hex.split("").map(c => c + c).join("");
+    }
+
     const r = parseInt(hex.substring(0, 2), 16);
     const g = parseInt(hex.substring(2, 4), 16);
     const b = parseInt(hex.substring(4, 6), 16);
